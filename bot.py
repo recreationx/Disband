f79583944052ab6d2173c534ee2e00475a29a3ee
unknown <nanzheng03@gmail.com>
2020-06-18 20:22:09 +0800

    Initial commit

---
+++ bot.py
@@ -0,0 +1,31 @@
+import discord
+import os
+from discord.ext import commands
+
+token = "NzIzMTMzMDA1NDY3NzQ2MzY1.XutOLQ.4UQ2KCrFugbfjXw6W6YuOafQUas"
+
+client = commands.Bot(command_prefix="$")
+
+
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f"cogs.{extension}")
+
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f"cogs.{extension}")
+
+
+@client.command()
+async def reload(ctx, extension):
+    client.unload_extension(f"cogs.{extension}")
+    client.load_extension(f"cogs.{extension}")
+
+
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py"):
+        client.load_extension(f"cogs.{filename[:-3]}")
+
+
+client.run(token)
